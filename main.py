81325e60b3264bb45522630dd7e6c814ec6ec24c
Hao Cauchy <quan.hovuong@gmail.com>
2019-12-14 10:51:30 -0800

    limit mem usage by ray

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ import logging
 ray.init(
     # If true, then output from all of the worker processes on all nodes will be directed to the driver.
     log_to_driver=True,
-    logging_level=logging.WARNING
+    logging_level=logging.WARNING,
+
+    # The amount of memory (in bytes)
+    object_store_memory=1073741824, # 1g
+    redis_max_memory=1073741824 # 1g
 )
 
 
